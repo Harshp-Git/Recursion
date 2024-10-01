2d6d645d5b95e1e351f4cfbd93db41b0e5b01a77
Harshp-Git <Parmarharsh8489012@gmail.com>
2024-10-01 23:58:20 +0530

    recursion

---
+++ Knight's_Tour.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int KnightTour(x,y,move_i,solution,x_moves,y_moves){
+    if (move_i == n*n) return true;
+    for(int k =0;k<=7;k++){
+        next_x=x+x_moves[k];
+        next_y=y+y_moves[k];
+    }
+    if (isvaildmove(next_x,next_y,solution)){
+        solution
+    }
+
+}
+int KnightTour(){
+    n=8
+
+}
+
+int main(){
+    
+    return 0;
+}
