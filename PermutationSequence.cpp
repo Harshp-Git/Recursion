0bb5ebe33703dfce9c9ddcede13e070b0b91776d
Harshp-Git <Parmarharsh8489012@gmail.com>
2024-10-03 22:20:30 +0530

    Recursion

--- PermutationSequence.cpp
+++ PermutationSequence.cpp
@@ -1,44 +1,35 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
-
-class Solution {
-public:
-    string helper(string str, int k, string ans) {
+    string helper(string str,int k,string ans){
         int n = str.length();
-        if (n == 0) return ans;
+        if(n==0) return ans;
         int fact = 1;
-        for (int i = 2; i <= n - 1; i++) {
+        for(int i=2;i<=n-1;i++){
             fact *= i;
         }
-        int idx = k / fact;
-        if (k % fact == 0) idx--;
-        char ch = str[idx];
-        string left = str.substr(0, idx);
-        string right = str.substr(idx + 1);
-        int q = 1;
-        if (k % fact == 0) q = fact;
-        else q = k % fact;
-        return helper(left + right, q, ans + ch);
+        int idx = k/fact;
+        if(k%fact==0) idx--;
+        char ch=str[idx];
+        string left = str.substr(0,idx);
+        string right = str.substr(idx+1);
+        int q=1;
+        if(k%fact==0) q=fact;
+        else q=k%fact;
+        return helper(left+right,q,ans+ch);
     }
-
     string getPermutation(int n, int k) {
-        string str = "";
-        for (int i = 1; i <= n; i++) {
-            str += to_string(i);
+        string str="";
+        for(int i=1;i<=n;i++){
+            str+= to_string(i);
         }
-        return helper(str, k - 1, "");
+        return helper(str,k,"");
+        
     }
-};
-
-int main() {
-    int n, k;
-    cout << "Enter the value of n: ";
-    cin >> n;
-    cout << "Enter the value of k: ";
-    cin >> k;
-    Solution solution;
-    string permutation = solution.getPermutation(n, k);
-    cout << "The " << k << "th permutation of the numbers from 1 to " << n << " is: " << permutation << endl;
+    int main(){
+        int n,k;
+        cin>>n>>k;
+        cout<<getPermutation(n, k);
+        
     return 0;
 }
